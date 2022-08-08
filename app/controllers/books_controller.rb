8a42f51456106b0e2c2e09dce6cbf2d32fdf18b4
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-08-08 05:50:21 +0000

    first-submission

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(update_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def update_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
